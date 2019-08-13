80f8baff62b6508218082f1cf34160403b6be07e
Wekoslav Stefanovski <swekster@gmail.com>
2019-08-13 23:39:49 +0200

    Added first class

---
+++ g6/Class01/WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Service;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("{op}/{first}/{second}")]
+        public ActionResult<OperationResult> Get(string op, int first, int second)
+        {
+            var calculator = new Calculator();
+            if (!calculator.CanMapToOperator(op)) {
+                return UnprocessableEntity($"Cannot map {op} to a valid operation");
+            }
+            
+            var operation = new Operation
+            {
+                Operator = calculator.MapStringToOperator(op),
+                FirstArgument = first,
+                SecondArgument = second
+            };
+            return calculator.ExecuteOperation(operation);
+        }
+    }
+}
---
+++ g6/Class01/WebApplication1/Models/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Operation
+    {
+        public Operator Operator { get; set; }
+        public int FirstArgument { get; set; }
+        public int SecondArgument { get; set; }
+    }
+
+    public class OperationResult: Operation
+    {
+        public int Result { get; set; }
+    }
+}
