adca289e4acdad4e63b341d27aefd2d6582df67b
dragan <dragang@iborn.net>
2019-08-15 21:01:27 +0200

    Class 2 finito

---
+++ g5/Class 2/Homework.md
@@ -0,0 +1,16 @@
+# Exercise
+Create an API controller for Users. A user should have:
+* First Name
+* Last Name
+* Age
+The user controller should have the following options:
+* static list with 3 or more users in it
+* get all users
+* get user by index
+* check if a user by ID is an adult
+* if a user is not found give the corresponding NOT FOUND status and a message
+
+Bonus
+* Add a form in the html for new user
+* Create a method in the controller for saving user to the list
+* connect the form and the action
--- g5/Class 2/SEDC.G5.Class2/SEDC.G5.Class2/Controllers/NoteObjectsController.cs
+++ g5/Class 2/SEDC.G5.Class2/SEDC.G5.Class2/Controllers/NoteObjectsController.cs
@@ -45,5 +45,75 @@ namespace SEDC.G5.Class2.Controllers
                 }
             }
         };
+
+        [HttpGet]
+        public ActionResult<List<Note>> Get()
+        {
+            return notes;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Note> Get(int id)
+        {
+            try
+            {
+                return notes[id - 1];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound("Note with that Id does not exist!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("BROKEN REQUEST - " + ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            string body;
+            using (StreamReader sr = new StreamReader(Request.Body))
+            {
+                body = sr.ReadToEnd();
+            }
+            Note note = JsonConvert.DeserializeObject<Note>(body);
+            notes.Add(note);
+            return Ok($"Note with id {notes.Count - 1} has been added!");
+        }
+
+        [HttpGet("{id}/tags")]
+        public ActionResult<List<Tag>> Tags(int id)
+        {
+            try
+            {
+                return notes[id - 1].Tags;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound("Note with that Id does not exist!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("BROKEN REQUEST - " + ex.Message);
+            }
+        }
+
+        [HttpGet("{noteId}/tags/{tagId}")]
+        public ActionResult<Tag> Tags(int noteId, int tagId)
+        {
+            try
+            {
+                return notes[noteId - 1].Tags[tagId - 1];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound("Note or tag with that Id does not exist!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("BROKEN REQUEST - " + ex.Message);
+            }
+        }
     }
 }
