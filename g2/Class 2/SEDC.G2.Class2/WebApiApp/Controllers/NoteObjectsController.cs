d52e1f868ae676965f3f05f315163eddaf30311b
dragan <dragang@iborn.net>
2019-08-16 19:38:54 +0200

    G2 Class 2 code

---
+++ g2/Class 2/SEDC.G2.Class2/WebApiApp/Controllers/NoteObjectsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiApp.Controllers
+{
+    public class Note
+    {
+        public string Text { get; set; }
+        public string Color { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+    public class Tag
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteObjectsController : ControllerBase
+    {
+        public static List<Note> notes = new List<Note>()
+        {
+            new Note(){ Text = "Don't forget to water the plant", Color = "blue", Tags = new List<Tag>()
+                {
+                    new Tag(){ Name = "Home", Color = "cyan"},
+                    new Tag(){ Name = "Priority Low", Color = "green"}
+                }
+            },
+            new Note(){ Text = "Drink more Tea", Color = "blue", Tags = new List<Tag>()
+            {
+                    new Tag(){ Name = "Misc", Color = "orange"},
+                    new Tag(){ Name = "Priority Low", Color = "green"}
+                }
+            },
+            new Note(){ Text = "Make a break every 1h of coding", Color = "blue", Tags = new List<Tag>()
+            {
+                    new Tag(){ Name = "work", Color = "blue"},
+                    new Tag(){ Name = "Priority Medium", Color = "yellow"}
+                }
+            }
+        };
+    }
+}
---
+++ g2/Class 2/SEDC.G2.Class2/WebApiApp/wwwroot/script-obj.js
@@ -0,0 +1,74 @@
+﻿let getAllBtn = document.getElementById("btn1");
+let getByIdBtn = document.getElementById("btn2");
+let addNoteBtn = document.getElementById("btn3");
+let getAllTagsBtn = document.getElementById("btn4");
+let getTagByIdBtn = document.getElementById("btn5");
+let getByIdInput = document.getElementById("input2");
+let addNoteInput1 = document.getElementById("input31");
+let addNoteInput2 = document.getElementById("input32");
+let addNoteInput3 = document.getElementById("input33");
+let getAllTagsInput = document.getElementById("input4");
+let getTagByIdInput1 = document.getElementById("input51");
+let getTagByIdInput2 = document.getElementById("input52");
+
+let port = "53222"
+let getAllNotes = async () => {
+    let url = "http://localhost:" + port + "/api/noteobjects";
+
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+};
+
+let getNoteById = async () => {
+    let url = "http://localhost:" + port + "/api/noteobjects/" + getByIdInput.value;
+
+    let response = await fetch(url);
+    let data = await response.text();
+    console.log(data);
+};
+
+let addNote = async () => {
+    let url = "http://localhost:" + port + "/api/noteobjects";
+    let getTags = (str) => str.split(",").reduce(function (x, y, index) {
+        let values = y.trim().split("-");
+        x.push({ name: values[0], color: values[1] });
+        return x;
+    }, []);
+    let note = { text: addNoteInput1.value, color: addNoteInput2.value, tags: getTags(addNoteInput3.value) }
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+    });
+}
+
+let getAllTags = async () => {
+    let url = "http://localhost:" + port + "/api/noteobjects/" + getAllTagsInput.value + "/tags";
+
+    let response = await fetch(url);
+    try {
+        let data = await response.json();
+        console.log(data);
+    } catch (e) {
+        console.log("Problem with request")
+    }
+
+};
+
+let getTagById = async () => {
+    let url = "http://localhost:" + port + "/api/noteobjects/" + getTagByIdInput1.value + "/tags/" + getTagByIdInput2.value;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+};
+
+
+getAllBtn.addEventListener("click", getAllNotes);
+getByIdBtn.addEventListener("click", getNoteById);
+addNoteBtn.addEventListener("click", addNote);
+getAllTagsBtn.addEventListener("click", getAllTags);
+getTagByIdBtn.addEventListener("click", getTagById);
