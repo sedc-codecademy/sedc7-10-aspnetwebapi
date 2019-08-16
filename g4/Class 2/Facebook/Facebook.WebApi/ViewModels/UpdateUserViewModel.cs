976bb33a39808dc0a5c6dda548c4d50358a4ec51
Stojancho Jefremov <stojancho.jefremov@seavus.com>
2019-08-16 21:11:16 +0200

    Class 2 finished

---
+++ g4/Class 2/Facebook/Facebook.WebApi/ViewModels/UpdateUserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Facebook.WebApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Facebook.WebApi.ViewModels
+{
+    public class UpdateUserViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public DateTime? Birthday { get; set; }
+
+        [Required]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Gender? Gender { get; set; }
+    }
+}
