6d618af7f4019798caea8bdb4e9815e2270af524
Wekoslav Stefanovski <swekster@gmail.com>
2019-08-19 21:59:27 +0200

    Class 04

---
+++ g6/Class04/BooksApi/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Model
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
