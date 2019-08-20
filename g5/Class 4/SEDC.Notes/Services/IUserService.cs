36043818327f7b1ce172ce132dde24e2221698f6
dragan <dragang@iborn.net>
2019-08-20 21:08:24 +0200

    G5 Class 4 finito

---
+++ g5/Class 4/SEDC.Notes/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public interface IUserService
+    {
+        UserModel Authenticate(string username, string password);
+        void Register(RegisterModel model);
+    }
+}
