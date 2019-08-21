e3ef37614720e3c84b2aaed195e3dacbf3b4a620
Wekoslav Stefanovski <swekster@gmail.com>
2019-08-21 20:12:22 +0200

    Added db example and script

---
+++ g6/Class05/BooksModels/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BooksModels
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfDeath { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
