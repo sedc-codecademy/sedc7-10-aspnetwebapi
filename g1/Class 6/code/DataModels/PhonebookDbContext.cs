28e62316da5857b34ab0e197935eb672fa77402c
dblazeski96 <dblazeski96@gmail.com>
2019-08-24 12:42:03 +0200

    add class 6 code

---
+++ g1/Class 6/code/DataModels/PhonebookDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels
+{
+    public class PhonebookDbContext : DbContext
+    {
+        public PhonebookDbContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<DtoUser> Users { get; set; }
+        public DbSet<DtoContact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DtoContact>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Contacts)
+                .HasForeignKey(c => c.UserId);
+        }
+    }
+}
---
+++ g1/Class 6/code/Repository/ContactRepository.cs
@@ -0,0 +1,52 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class ContactRepository : IRepository<DtoContact>
+    {
+        private readonly PhonebookDbContext _db;
+        public ContactRepository(PhonebookDbContext context)
+        {
+            _db = context;
+        }
+
+        public IEnumerable<DtoContact> GetAll()
+        {
+            return _db.Contacts.ToList();
+        }
+
+        public void Add(DtoContact entity)
+        {
+            _db.Contacts.Add(entity);
+
+            _db.SaveChanges();
+        }
+
+
+        public void Update(DtoContact entity)
+        {
+            var contact = _db.Contacts
+                .SingleOrDefault(c => c.Id == entity.Id && c.UserId == entity.UserId);
+
+            contact.FirstName = entity.FirstName;
+            contact.LastName = entity.LastName;
+            contact.Email = entity.Email;
+            contact.Address = entity.Address;
+            contact.PhoneNumber = entity.PhoneNumber;
+            contact.UserId = entity.UserId;
+
+            _db.SaveChanges();
+        }
+
+        public void Delete(DtoContact entity)
+        {
+            _db.Contacts.Remove(entity);
+
+            _db.SaveChanges();
+        }
+    }
+}
