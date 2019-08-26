70226d46685edc1aac0e1ac7fc87d6e774155b63
Stojancho Jefremov <stojancho.jefremov@seavus.com>
2019-08-26 21:17:01 +0200

    Class 7 added

---
+++ g4/Class 7/SEDC.Loto3000/SEDC.Loto3000.Models/Settings/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace SEDC.Loto3000.Models.Settings
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; }
+
+        public bool ValidateIssuerSigningKey { get; set; }
+
+        public bool ValidateAudience { get; set; }
+
+        public bool ValidateIssuer { get; set; }
+
+        public bool RequireHttpsMetadata { get; set; }
+
+        public bool SaveToken { get; set; }
+    }
+}
---
+++ g4/Class 7/SEDC.Loto3000/SEDC.Loto3000.WebApi/Controllers/TicketController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SEDC.Loto3000.BusinessLayer.Contracts;
+using SEDC.Loto3000.WebApi.ViewModels;
+using System.Linq;
+using System.Security.Claims;
+
+namespace SEDC.Loto3000.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ticket")]
+    [Authorize(Roles = "NotAdmin")]
+    public class TicketController : Controller
+    {
+        private readonly ITicketService _ticketService;
+        private readonly IConfiguration _configuration;
+
+        public TicketController(ITicketService ticketService, IConfiguration configuration)
+        {
+            _ticketService = ticketService;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult SubmitTicket([FromBody]SubmitTicketViewModel bodyRequest)
+        {
+            var email = User.Claims
+                            .SingleOrDefault(c => c.Type == ClaimTypes.Email)
+                            ?.Value;
+            var addedTicket = _ticketService.SubmitTicket(bodyRequest.PickedNumbers, email);
+            var apiBaseUrl = _configuration.GetValue<string>("ApiBaseUrl");
+            var response = new
+            {
+                WinnersBoardLink = $"{apiBaseUrl}/api/winner?drawId={addedTicket.DrawId}",
+                Message = "You should wait for the draw and if you get a prize you will see your name on the board"
+            };
+            var ticketLink = $"{apiBaseUrl}/api/ticket/{addedTicket.Id}";//TODO: implement this(HttpGet action to read specific ticket)
+            return Created(ticketLink, response);
+        }
+    }
+}
