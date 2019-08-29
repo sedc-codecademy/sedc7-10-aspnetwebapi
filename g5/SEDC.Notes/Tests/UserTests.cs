51c95fec184684a454f61dee5b8e95a6184d7a47
dragan <dragang@iborn.net>
2019-08-29 10:08:19 +0200

    G5 Class 8 init

---
+++ g5/SEDC.Notes/Tests/UserTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
