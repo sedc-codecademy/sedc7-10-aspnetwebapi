a8c3cea37c9f0ea818bc76fccfcee05582e04092
Dejan <djovanov92@gmail.com>
2019-09-06 09:50:26 +0200

    cors config added

--- g2/SEDC.Notes/WebApi/Startup.cs
+++ g2/SEDC.Notes/WebApi/Startup.cs
@@ -23,6 +23,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             var appConfig = Configuration.GetSection("AppSettings");
@@ -61,7 +62,13 @@ namespace WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(options =>
+            {
+                options.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithExposedHeaders("Authorization");
+            });
             app.UseAuthentication();
             app.UseMvc();
         }
